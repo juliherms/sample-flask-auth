bf1578498c2025f0602349f76fa9a7a0e27d8d20
Juliherms De Lima Vasconcelos <juliherms.vasconcelos@exmed.com.br>
2024-07-28 19:34:09 -0300

    feat: criacao do login, logou e cadastro de usuario

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import (
+    LoginManager,
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+)
 
 
 app = Flask(__name__)
@@ -12,12 +18,19 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
-# view login
-
 
 @app.route("/hello-worLd", methods=["GET"])
 def hello_world():
     return "Hello world"
+
+
+# view login - associa o login a rota de login
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 @app.route("/login", methods=["POST"])
@@ -30,9 +43,35 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({"message": "Autenticação realizada com sucesso!"})
 
     return jsonify({"message": "Credenciais inválidas"}), 400
+
+
+@app.route("/logout", methods=["GET"])
+@login_required  # protege a rota, somente acesso com login realizado
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
+
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({"message": "Usuário cadastrado com sucesso!"})
+
+    return jsonify({"message": "Dados inválidos!"}), 400
 
 
 if __name__ == "__main__":
